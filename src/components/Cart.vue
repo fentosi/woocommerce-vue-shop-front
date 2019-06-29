ff768ef0d6018d65b333dce1ec468903eb22637b
Adam Fentosi <fentosi.adam@gmail.com>
2019-06-29 13:52:36 +0200

    Cart component for new order

---
+++ src/components/Cart.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Cart</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'cart'
+};
+</script>
--- src/views/OrderNew.vue
+++ src/views/OrderNew.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="container">
+  <div class="content">
     <h1>New Order</h1>
-    <template v-for="product in products">
-      <order-new-item v-bind="product" v-bind:key="product.id" />
-    </template>
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Product items</h3>
+        <template v-for="product in products">
+          <order-new-item v-bind="product" v-bind:key="product.id" />
+        </template>
+      </div>
+      <div class="col-md-4">
+        <cart />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,11 +19,12 @@
 import { LOAD_PRODUCTS } from '../store/actionTypes';
 import { mapState } from 'vuex';
 import OrderNewItem from '../components/OrderNewItem';
+import Cart from '../components/Cart';
 
 export default {
   name: 'orderNew',
   components: {
-    OrderNewItem
+    OrderNewItem, Cart
   },
   computed: {
     ...mapState(['products'])

faec156cb9dca8887eff2274a3ac57ddc75628cc
Adam Fentosi <fentosi.adam@gmail.com>
2019-06-29 16:45:39 +0200

    Add FontAwesomeIcon package

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,16 @@
 </template>
 
 <script>
+import Vue from 'vue';
 import Navbar from './components/Navbar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMinusSquare, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faMinusSquare, faPlusSquare);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 export default {
   name: 'App',
   components: { Navbar }

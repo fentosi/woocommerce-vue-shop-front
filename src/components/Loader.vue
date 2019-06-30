88857d4d6dabc5c5dddac97cfb9f7ab7df5f4c90
Adam Fentosi <fentosi.adam@gmail.com>
2019-06-30 11:49:15 +0200

    New Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex d-flex-row justify-content-center align-self-center">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  div.d-flex {
+    margin-top: 2em;
+  }
+</style>
+
+<script>
+
+export default {
+  name: 'loader'
+};
+</script>

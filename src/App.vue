8005db124e5cdb4c1f9d2dc56db397448b3133e3
Adam Fentosi <fentosi.adam@gmail.com>
2019-06-30 10:52:51 +0200

    Show Alert component if alert is set in store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
    <navbar></navbar>
+   <alert v-if="error" v-bind:message="error"></alert>
    <router-view></router-view>
   </div>
 </template>
@@ -11,6 +12,8 @@ import Navbar from './components/Navbar';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMinusSquare, faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Alert from './components/Alert';
+import { mapState } from 'vuex';
 
 library.add(faMinusSquare, faPlusSquare, faTrash);
 
@@ -18,6 +21,9 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 export default {
   name: 'App',
-  components: { Navbar }
+  components: { Navbar, Alert },
+  computed: {
+    ...mapState(['error'])
+  }
 };
 </script>

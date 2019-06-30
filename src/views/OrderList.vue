5fb94f6e0aed9e1190c71999d66b0054f9d0f781
Adam Fentosi <fentosi.adam@gmail.com>
2019-06-30 13:27:37 +0200

    Show loading indicator on orders list

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="orders-list container">
     <h1>Orders</h1>
-    <table class="table">
+    <loader v-if="isLoading" />
+    <table class="table" v-else-if="orders.length > 0">
       <thead>
-      <tr>
-        <th>#</th>
-        <th>Customer</th>
-        <th>Payment method</th>
-        <th>Total</th>
-        <th>Status</th>
-        <th>Date</th>
-        <th>Paid</th>
-      </tr>
+        <tr>
+          <th>#</th>
+          <th>Customer</th>
+          <th>Payment method</th>
+          <th>Total</th>
+          <th>Status</th>
+          <th>Date</th>
+          <th>Paid</th>
+        </tr>
+      </thead>
       <template v-for="order in orders">
         <order-list-item v-bind="order" v-bind:key="order.id"></order-list-item>
       </template>
-      </thead>
     </table>
+    <div v-else>
+      No orders
+    </div>
   </div>
 </template>
 
@@ -24,11 +28,18 @@
 import OrderListItem from './OrderListItem';
 import { LOAD_ORDERS } from '../store/actionTypes';
 import { mapState } from 'vuex';
+import Loader from '../components/Loader';
+import { SET_ERROR } from '../store/mutationTypes';
 
 export default {
   name: 'orderList',
+  data() {
+    return {
+      isLoading: false
+    };
+  },
   components: {
-    OrderListItem
+    OrderListItem, Loader
   },
   computed: {
     ...mapState(['orders'])
@@ -37,8 +48,15 @@ export default {
     this.loadOrders();
   },
   methods: {
-    loadOrders() {
-      this.$store.dispatch(LOAD_ORDERS);
+    async loadOrders() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch(LOAD_ORDERS);
+      } catch (e) {
+        this.$store.commit(SET_ERROR, 'Something went wrong');
+      } finally {
+        this.isLoading = false;
+      }
     }
   }
 };
--- tests/unit/components/OrderList.spec.js
+++ tests/unit/components/OrderList.spec.js
@@ -2,10 +2,13 @@ import { shallowMount } from '@vue/test-utils';
 import OrderList from '../../../src/views/OrderList';
 import OrderListItem from '../../../src/views/OrderListItem';
 import { createStore } from '../../../src/store';
+import { SET_ERROR } from '../../../src/store/mutationTypes';
+import Loader from '../../../src/components/Loader';
 
 describe('OrdersList.vue', () => {
   let component;
   let store;
+  let storeCommitSpy;
 
   const data = [{
     id: 123,
@@ -21,7 +24,8 @@ describe('OrdersList.vue', () => {
   }];
 
   beforeEach(() => {
-    store = store = createStore();
+    store = createStore();
+    storeCommitSpy = jest.spyOn(store, 'commit');
     store.state.orders = data;
     component = shallowMount(OrderList, {
       mocks: {
@@ -30,7 +34,26 @@ describe('OrdersList.vue', () => {
     });
   });
 
-  it('renders ordersRepository list items', () => {
+  it('renders ordersRepository list items', async () => {
+    await component.vm.loadOrders();
     expect(component.contains(OrderListItem)).toBe(true);
+  });
+
+  it('sets loading on loadOrders', async () => {
+    const promise = component.vm.loadOrders();
+
+    expect(component.vm.isLoading).toEqual(true);
+    expect(component.contains(Loader)).toBe(true);
+
+    await promise;
+
+    expect(component.vm.isLoading).toEqual(false);
+    expect(component.contains(Loader)).toBe(false);
+  });
+
+  it('sets error if loading is failed', async () => {
+    await component.vm.loadOrders();
+
+    expect(storeCommitSpy).toHaveBeenCalledWith(SET_ERROR, 'Something went wrong');
   });
 });

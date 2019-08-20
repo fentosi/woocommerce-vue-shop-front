d0ee1acec07b0d70db3fb299aeab68a772b20b93
fentosi <fentosi.adam@gmail.com>
2019-08-20 11:42:21 +0200

    Update product repository to get all items in stock ordered by title

--- src/repositories/products.js
+++ src/repositories/products.js
@@ -4,7 +4,14 @@ export default {
   baseUrl: process.env.VUE_APP_WOOCOMMERCE_URL,
   basePath: 'api/products',
   getAll() {
-    return axios.get(this.getUrl(null, { status: 'publish' }));
+    return axios.get(this.getUrl(
+      null,
+      {
+        status: 'publish',
+        stock_status: 'instock',
+        orderby: 'title',
+        per_page: 100
+      }));
   },
   get(productID) {
     return axios.get(this.getUrl(productID));
@@ -19,7 +26,7 @@ export default {
     if (parameters) {
       url += '?';
       for (const key of Object.keys(parameters)) {
-        url += encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key]);
+        url += encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key]) + '&';
       }
     }
 
--- tests/unit/repositories/Products.spec.js
+++ tests/unit/repositories/Products.spec.js
@@ -50,7 +50,7 @@ describe('Products Repository', () => {
 
   it('"getAll" makes a call to productRepository endpoint', () => {
     const spy = jest.spyOn(axios, 'get').mockImplementation(() => [product]);
-    const url = woocommerceUrl + '/' + basePath + '?status=publish';
+    const url = woocommerceUrl + '/' + basePath + '?status=publish&stock_status=instock&orderby=title&per_page=100&';
 
     let ordersData = productRepository.getAll();
     expect(spy).toHaveBeenCalledWith(url);
@@ -78,12 +78,12 @@ describe('Products Repository', () => {
       });
 
       it('with parameters returns url with parameters', () => {
-        let expectedUrl = woocommerceUrl + '/' + basePath + '?asd=123';
+        let expectedUrl = woocommerceUrl + '/' + basePath + '?asd=123&';
         expect(productRepository.getUrl(null, { asd: 123 })).toBe(expectedUrl);
       });
 
       it('with parameters returns url with encoded parameters', () => {
-        let expectedUrl = woocommerceUrl + '/' + basePath + '?asd%3Dasd=12%20%2B%203';
+        let expectedUrl = woocommerceUrl + '/' + basePath + '?asd%3Dasd=12%20%2B%203&';
         expect(productRepository.getUrl(null, { 'asd=asd': '12 + 3' })).toBe(expectedUrl);
       });
     });
